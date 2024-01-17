b6b04f8a01406af8338fd48bb2008c00d8a1b1a7
Kenneth Camacho <46813058+kentuy17@users.noreply.github.com>
2024-01-17 17:51:06 +0800

    partial fix reports

--- react/src/Report.jsx
+++ react/src/Report.jsx
@@ -1,17 +1,17 @@
 import {
   DashboardContextProvider,
-  useDashboardContext,
+  // useDashboardContext,
 } from "./context/DashboardContextProvider";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
 import { Toaster } from "@ui/sonner";
-import { DatePicker } from "./components/extra/date-picker";
+// import { DatePicker } from "./components/extra/date-picker";
 import { ReportCard } from "./components/extra/report-card";
-import * as XLSX from "xlsx";
-import { toast } from "sonner";
+// import * as XLSX from "xlsx";
+// import { toast } from "sonner";
 
 import moment from "moment";
-import axiosClient from "./axios-client";
+// import axiosClient from "./axios-client";
 import { secondsToHuman } from "./lib/timehash";
 import { useState } from "react";
 import { AlertDialogTemplate } from "./components/layout/alert-dialog-template";
@@ -34,29 +34,29 @@ export const getWorkDuration = (data, show = true) => {
 };
 
 function Report() {
-  const { date } = useDashboardContext();
-  const [selectedDate, setSelectedDate] = useState(date);
+  // const { date } = useDashboardContext();
+  // const [selectedDate, setSelectedDate] = useState(date);
   const [dialogOpen, setDialogOpen] = useState(false);
   const [selectedModule, setSelectedModule] = useState("attendance");
 
-  const handleDateChange = (newDate) => {
-    setSelectedDate(newDate);
-  };
+  // const handleDateChange = (newDate) => {
+  //   setSelectedDate(newDate);
+  // };
 
-  const formatExcelData = (data) => {
-    return data.map((d) => {
-      return {
-        ID: d.employee.employee_id,
-        NAME: `${d.employee.first_name} ${d.employee.last_name}`,
-        DATE: d.datein,
-        "TIME-IN": d.timein,
-        "TIME-OUT": d.timeout,
-        LATE: "--:--",
-        UNDERTIME: "--:--",
-        TOTAL: getWorkDuration(d, false),
-      };
-    });
-  };
+  // const formatExcelData = (data) => {
+  //   return data.map((d) => {
+  //     return {
+  //       ID: d.employee.employee_id,
+  //       NAME: `${d.employee.first_name} ${d.employee.last_name}`,
+  //       DATE: d.datein,
+  //       "TIME-IN": d.timein,
+  //       "TIME-OUT": d.timeout,
+  //       LATE: "--:--",
+  //       UNDERTIME: "--:--",
+  //       TOTAL: getWorkDuration(d, false),
+  //     };
+  //   });
+  // };
 
   const handleAppsExport = () => {
     setSelectedModule("applications");
@@ -93,33 +93,35 @@ function Report() {
   };
 
   const handleAttendanceExport = async () => {
-    const promise = () =>
-      new Promise((resolve, reject) => {
-        axiosClient
-          .get(
-            `/reports/attendance/${moment(selectedDate).format("YYYY-MM-DD")}`
-          )
-          .then((resp) => resolve(formatExcelData(resp.data.data)))
-          .catch((err) => reject(err));
-      });
+    setSelectedModule("attendance");
+    setDialogOpen(!dialogOpen);
+    // const promise = () =>
+    //   new Promise((resolve, reject) => {
+    //     axiosClient
+    //       .get(
+    //         `/reports/attendance/${moment(selectedDate).format("YYYY-MM-DD")}`
+    //       )
+    //       .then((resp) => resolve(formatExcelData(resp.data.data)))
+    //       .catch((err) => reject(err));
+    //   });
 
-    toast.promise(promise, {
-      loading: "Exporting attendance data...",
-      success: (resp) => {
-        const worksheet = XLSX.utils.json_to_sheet(resp);
-        const workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, worksheet, "Attendance");
-        XLSX.writeFile(workbook, "iNTrack-Attendance-Report.xlsx");
-        return `Successfully exported ${resp.length} records`;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-      action: {
-        label: "Close",
-        onClick: () => console.log("Event has been created"),
-      },
-    });
+    // toast.promise(promise, {
+    //   loading: "Exporting attendance data...",
+    //   success: (resp) => {
+    //     const worksheet = XLSX.utils.json_to_sheet(resp);
+    //     const workbook = XLSX.utils.book_new();
+    //     XLSX.utils.book_append_sheet(workbook, worksheet, "Attendance");
+    //     XLSX.writeFile(workbook, "iNTrack-Attendance-Report.xlsx");
+    //     return `Successfully exported ${resp.length} records`;
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //   },
+    //   action: {
+    //     label: "Close",
+    //     onClick: () => console.log("Event has been created"),
+    //   },
+    // });
   };
 
   return (
@@ -129,9 +131,9 @@ function Report() {
           <div className="space-y-1">
             <h2 className="text-3xl font-bold tracking-tight">Reports</h2>
           </div>
-          <div className="ml-auto mr-4">
+          {/* <div className="ml-auto mr-4">
             <DatePicker onDateChanged={handleDateChange} />
-          </div>
+          </div> */}
         </div>
         <Tabs defaultValue="generate" className="h-full space-y-6">
           <div className="space-between flex items-center">
