e1a740d28f31e0dd498a60e31e50f3f37bca7ea1
kennethcamacho <46813058+kentuy17@users.noreply.github.com>
2024-01-25 18:02:50 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'apiv2'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'apiv2/*'],
 
     'allowed_methods' => ['*'],
 
