1f7886322f75c07bb004fadc35fb701a82055b26
kennethcamacho <46813058+kentuy17@users.noreply.github.com>
2024-01-27 12:42:24 +0800

    logging

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class Cors
 {
@@ -14,14 +15,24 @@ class Cors
             'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
             'Access-Control-Allow-Headers' => 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range',
         ];
-        if ($request->getMethod() == "OPTIONS") {
-            //The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            // return response()->json('OK', 200, $headers);
-        }
+
         $response = $next($request);
         foreach ($headers as $key => $value) {
             $response->header($key, $value);
         }
+
+        if ($request->getMethod() != "OPTIONS") {
+            //The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            // return response()->json('OK', 200, $headers);
+            $details = [
+                'ip' => $request->ip(),
+                'user' => $request->user(),
+                'method' => $request->method(),
+                'mac_address' => exec('getmac')
+            ];
+            Log::channel('accesslog')->info(json_encode($details, JSON_PRETTY_PRINT));
+        }
+
         return $response;
     }
 }
