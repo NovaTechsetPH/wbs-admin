db302bfe3442ff18ab81477885cc595f40802ce7
kennethcamacho <46813058+kentuy17@users.noreply.github.com>
2024-02-22 14:12:50 +0800

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,9 +6,22 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Console\Events\CommandStarting;
 
 class EventServiceProvider extends ServiceProvider
 {
+    /**
+     * Command starting time
+     */
+    public $starting_time;
+
+    /**
+     * Command finished time
+     */
+    public $finished_time;
+
     /**
      * The event to listener mappings for the application.
      *
@@ -27,7 +40,17 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Event::listen(CommandStarting::class, function (CommandStarting $event) {
+            $this->starting_time = microtime(true);
+        });
+
+        Event::listen(CommandFinished::class, function (CommandFinished $event) {
+            $this->finished_time = microtime(true);
+            $time = (($this->finished_time - $this->starting_time)); // time in seconds
+
+            // here you can store, display or log time for future use.
+            Log::info("Command [$event->command] takes $time seconds.");
+        });
     }
 
     /**
