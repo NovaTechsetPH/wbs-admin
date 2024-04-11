a12d9605c4e1c943d3d80327e4de588a76780799
Kenneth C <kcamacho@novatechset.com>
2024-04-11 09:21:39 +0530

    fixed redis

--- app/Http/Controllers/Api/AppCategoriesController.php
+++ app/Http/Controllers/Api/AppCategoriesController.php
@@ -3,22 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreUserRequest;
-use App\Http\Requests\UpdateUserRequest;
-use App\Http\Resources\AppCategoriesResource;
 use App\Models\AppCategories;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 
 class AppCategoriesController extends Controller
 {
+    private $disabled_categories = [90];
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $categories = AppCategories::orderBy('priority_id', 'ASC')
+        $categories = AppCategories::whereNotIn('id', $this->disabled_categories)
+            ->orderBy('priority_id', 'ASC')
             ->orderBy('id', 'ASC')
             ->get();
 
@@ -27,14 +25,6 @@ class AppCategoriesController extends Controller
             'message' => 'Successfully retrieved all categories',
         ]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    // public function create()
-    // {
-    //     //
-    // }
 
     /**
      * Store a newly created resource in storage.
@@ -90,14 +80,6 @@ class AppCategoriesController extends Controller
             'message' => 'Successfully retrieved category',
         ]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    // public function edit(string $id)
-    // {
-    //     //
-    // }
 
     /**
      * Update the specified resource in storage.
